44e2b9750b067c4016a781ae7947f3847c0a1bec
Donald Biggers <DuGsEmail@yahoo.com>
2021-11-13 20:13:40 -0600

    time-block alignment and style

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,29 +7,28 @@ var tableList = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm
 function timeContainer() {
 currentDay();
 var timeContainer = document.getElementById("timeBox");
-//timeContainer.innerHTML = "BLAHBLAHBLAH";
-//timeContainer.style.backgroundColor="red";
 var createTable = document.createElement("table");
 for (let index = 0; index < tableList.length; index++) {
     const element = tableList[index];
     var createRow = document.createElement("tr");
-    createRow.setAttribute("class", "row");
+    //createRow.setAttribute("class", "row");
     var cellOne = document.createElement("td");
     cellOne.setAttribute("class", "hour");
     cellOne.innerHTML = element;
     var cellTwo = document.createElement("td");
     var cellText = document.createElement("textarea");
-    cellText.setAttribute("class", "description");
+    //cellText.setAttribute("class", "description");
     cellTwo.appendChild(cellText);
+    cellTwo.setAttribute("class", "past");
     var cellThree = document.createElement("td");
-    cellThree.setAttribute("class", "time-block saveBtn");
+    cellThree.setAttribute("class", "saveBtn");
     //cellThree.innerHtml = "";
      var cellSave = document.createElement("input");
      cellSave.setAttribute("type", "image");
      cellSave.setAttribute("src", "/assets/images/1x/outline_save_black_24dp.png");
      cellSave.setAttribute("id", "save" + index);
      cellSave.setAttribute("name", "save" + index);
-     cellSave.setAttribute("class", "saveBtn");
+    // cellSave.setAttribute("class", "saveBtn");
 //     cellSave.innerHTML = mdiContentSaveOutline; 
     cellThree.appendChild(cellSave);
     createRow.appendChild(cellOne);
