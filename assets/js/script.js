2650df9831069905b0df7c8305a6f3a48ffea643
Donald Biggers <DuGsEmail@yahoo.com>
2021-11-14 13:34:33 -0600

    finished functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,6 @@
-//import { mdiContentSaveOutline } from '@mdi/js';
 var tableList = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"
 
 ];
-
 
 function timeContainer() {
 currentDay();
@@ -11,16 +9,22 @@ var createTable = document.createElement("table");
 for (let index = 0; index < tableList.length; index++) {
     const element = tableList[index];
     var createRow = document.createElement("tr");
-    //createRow.setAttribute("class", "row");
     var cellOne = document.createElement("td");
     cellOne.setAttribute("class", "hour");
     cellOne.innerHTML = element;
     var cellTwo = document.createElement("td");
     var cellText = document.createElement("textarea");
-    //cellText.setAttribute("cols", "63");
+    cellText.setAttribute("id", "text" + index);
+    var eventCall = localStorage.getItem("callEvent");
+    if (eventCall === null) {
+        var callArray = [];
+    } else {
+        var callArray = JSON.parse(eventCall);
+        cellText.innerHTML = callArray [index];
+        console.log(index);
+    }
 
     cellTwo.appendChild(cellText);
-    //cellTwo.setAttribute("class", "past");
     var hour = moment().hour();
     if (hour === index + 9) {
         cellTwo.setAttribute("class", "present");
@@ -30,17 +34,16 @@ for (let index = 0; index < tableList.length; index++) {
         cellTwo.setAttribute("class", "future");
     }
 
-   // var momentDisplay = moment(hour).format("dddd, MMMM Do");
     var cellThree = document.createElement("td");
     cellThree.setAttribute("class", "saveBtn");
-    //cellThree.innerHtml = "";
      var cellSave = document.createElement("input");
      cellSave.setAttribute("type", "image");
      cellSave.setAttribute("src", "/assets/images/1x/outline_save_black_24dp.png");
      cellSave.setAttribute("id", "save" + index);
      cellSave.setAttribute("name", "save" + index);
-    // cellSave.setAttribute("class", "saveBtn");
-//     cellSave.innerHTML = mdiContentSaveOutline; 
+     cellSave.addEventListener("click", function() {
+         save(index);
+     });
     cellThree.appendChild(cellSave);
     createRow.appendChild(cellOne);
     createRow.appendChild(cellTwo);
@@ -48,6 +51,19 @@ for (let index = 0; index < tableList.length; index++) {
     createTable.appendChild(createRow);
 }
 timeContainer.appendChild(createTable);
+}
+function save(index) {
+    var name = document.getElementById("text" + index).value;
+   
+    var eventCall = localStorage.getItem("callEvent");
+    if (eventCall === null) {
+        var callArray = ["", "", "", "", "", "", "", "", ""];
+    } else {
+        var callArray = JSON.parse(eventCall);
+    } callArray [index] = name;
+    localStorage["callEvent"] = JSON.stringify(callArray);
+
+    timeContainer();
 }
 
 function currentDay() {
